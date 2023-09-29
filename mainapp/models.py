d507a7c81f5339f0678d6a5bf8b3fa781c981ee3
EnzoF <enzo.fritzler@gmail.com>
2023-09-29 12:55:21 -0300

    Primer esqueleto de pagina funcional

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    titulo = models.CharField(max_length=50)
+    numero = models.IntegerField()
+
+class Ejemplo(models.Model):
+    post = models.CharField(max_length=50)
+    texto = models.TextField()
+    
+ 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
+from mainapp.models import Curso
 
 # Create your views here.
+
+def inicio(request):
+    datos= {
+        'fecha': datetime.now()
+    }    
+    return render(request, r'mainapp\inicio.html', datos)
+def crear_curso(request,titulo,numero):
+    curso = Curso(titulo = titulo, numero=numero)
+    curso.save()
+    
+    return(render(request, r'mainapp\curso_creado.html', {}))
